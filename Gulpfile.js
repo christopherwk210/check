ea7963d3899e0c7b682d95e4364c5d32d002ecfb
Chris Anselmo <canselmo@hybridhealth.com>
2017-06-13 08:18:33 -0400

    Added sourcemaps to gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require("gulp");
+var sourcemaps = require("gulp-sourcemaps");
 var babel = require("gulp-babel");
+var concat = require("gulp-concat");
 
 gulp.task("default", function () {
   return gulp.src("src/app.js")
+    .pipe(sourcemaps.init())
     .pipe(babel())
+    .pipe(sourcemaps.write("."))
     .pipe(gulp.dest("dist"));
 });
